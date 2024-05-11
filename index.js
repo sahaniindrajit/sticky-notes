52b78db891facfc4717d0e51cd21a32d6bf7bd9a
Indrajit sahani <indrajit38mig@gmail.com>
2024-05-11 19:26:10 +0530

    stores history

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var container2=document.getElementsByClassName("container2")[0];
 var container3=document.getElementsByClassName("container3")[0];
+var container=document.getElementsByClassName(".success");
 var checkIcon=document.getElementById("check-icon");
 var xIcon=document.getElementById("x-icon");
 var i=0;
+
 xIcon.addEventListener("click",function(){
     typeNote();
 })
 
 checkIcon.addEventListener("click",function(){
-    createNote();
+    addNotes();
 })
+
+getNotes().forEach((note) => {
+    const noteElement = createNote(note.id, note.content);
+
+    container2.insertBefore(noteElement, container2.firstChild);
+});
 
 function typeNote(){
     if(container3.style.display=="none"){
@@ -20,47 +28,15 @@ function typeNote(){
     }
 }
 
-function createNote(){
-    var noteText=document.getElementById("note-text").value;
-    var node0=document.createElement("div");
-    var node1=document.createElement("h1");
 
-    node1.innerHTML=noteText;
-    node1.setAttribute("style","width:250px ; height:250px ; font-size:26px ; padding:25px; margin-top:10px; overflow:hidden; box-shadow:0px 10px 24px 0px rgba(0,0,0,0.75)");
-    node1.style.margin=margin();
-    node1.style.transform=rotate();
-    node1.style.background=color();
-
-    node0.appendChild(node1);
-    container2.insertAdjacentElement("beforeend",node0);
-
-    node0.addEventListener("mouseenter",function(){
-        node0.style.transform="scale(1.1)";
-    })
-
-    node0.addEventListener("mouseleave",function(){
-        node0.style.transform="scale(1)";
-    
-    })
-
-    node0.addEventListener("dblclick",function(){
-        node0.remove();
-    })
-
-    document.getElementById("note-text").value='';
-
-}
 
 function margin(){
-    var random_margin=["-5px","1px","5px","12px","3px","9px","15px"];
+    var random_margin=["10px","13px","15px","12px","13px","16px","17px"];
     return random_margin[Math.floor(Math.random()*random_margin.length)];
 
 }
 
-function rotate(){
-    var random_rotate=["rotate(3deg)","rotate(-4deg)","rotate(7deg)","rotate(-11deg)","rotate(18deg)","rotate(-13deg)"];
-    return random_rotate[Math.floor(Math.random()*random_rotate.length)];
-}
+
 
 function color(){
     var random_color=["#219866","#51c30f","#70a85f","#85cadb","#2390b5","#46fb58","#58759f","#e72e20"];
@@ -68,4 +44,79 @@ function color(){
         i=0;
     }
     return random_color[i++];
+}
+
+function getNotes(){
+    return JSON.parse(localStorage.getItem("notes-history") || "[]" );
+}
+
+
+function saveNotes(notes){
+    localStorage.setItem("notes-history",JSON.stringify(notes));
+}
+
+function createNote(id,content){
+    var node1=document.createElement("textarea");
+
+    node1.value=content;
+    node1.setAttribute("style","width:200px ; height:200px ; font-size:26px ; padding:25px; margin-top:10px; overflow:hidden; box-shadow:0px 10px 24px 0px rgba(0,0,0,0.75)");
+    node1.style.margin=margin();
+    
+    node1.style.background=color();
+
+
+    node1.addEventListener("change",()=>{
+        updateNote(id,node1.value);
+    })
+
+    node1.addEventListener("mouseenter",function(){
+        node1.style.transform="scale(1.1)";
+    })
+
+    node1.addEventListener("mouseleave",function(){
+        node1.style.transform="scale(1)";
+    
+    })
+
+    node1.addEventListener("dblclick",function(){
+        deleteNote(id,node1);
+    })
+
+    
+
+    container2.insertBefore(node1, container2.firstChild);
+    document.getElementById("note-text").value='';
+
+    return node1;
+
+}
+
+
+function addNotes(){
+    const existingNotes=getNotes();
+    const noteObject={
+        id:Math.floor(Math.random()*100000),
+        content:document.getElementById("note-text").value
+    };
+
+    createNote(noteObject.id,noteObject.content);
+
+    
+    existingNotes.push(noteObject);
+    saveNotes(existingNotes);
+}
+
+function updateNote(id,newContent){
+    const notes=getNotes();
+    const targetNote=notes.filter((note)=>note.id===id)[0];
+
+    targetNote.content=newContent;
+    saveNotes(notes);
+}
+
+function deleteNote(id,element){
+    const notes=getNotes().filter(note=>note.id !=id);
+
+    saveNotes(notes);
+    container2.removeChild(element);
 }
